fbf1b2e8b71a629bf733b817ba1fdf4868e133c5
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-06-21 21:17:43 +0100

    Finished the page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,13 +2,19 @@ import "./Hero.css"
 const Hero = () => {
     return (
         <div className="hero-section">
-            <main className="left">
-                <h1>Make remote work</h1>
+            <main className="left-hero">
+                <h1>Make <br /> remote work</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat debitis perferendis tempora non earum, ipsam eius, eum accusantium odit molestias repellat nostrum voluptate veniam, sint neque quo. Vel, rem facilis.</p>
                 <button>Learn more</button>
+                <div className="partners">
+                    <img src="./databiz.svg" alt="databliz" />
+                    <img src="./audiophile.svg" alt="audiophile" />
+                    <img src="./meet.svg" alt="meet" />
+                    <img src="./maker.svg" alt="maker" />
+                </div>
             </main>
-            <main className="right">
-                <img src="./image-hero-desktop.png" alt="" />
+            <main className="right-hero">
+                <img src="./heroImage.png" alt="hero pic" />
             </main>
         </div>
     )
